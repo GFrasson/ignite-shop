191578adf42a9dce16f9bb26aba09352766a6c43
Gabriel Frasson <bielfc27@gmail.com>
2023-04-13 11:48:55 -0300

    Config global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+globalStyles();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header>
+        
+      </header>
+
+      <Component {...pageProps} />
+    </div>
+  )
 }
