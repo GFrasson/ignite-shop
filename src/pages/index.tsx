2dddf07b571fa1c1ec23190501c094bd4b3e0df4
Gabriel Frasson <bielfc27@gmail.com>
2023-04-13 15:17:28 -0300

    Home page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
-import { styled } from "@/styles"
+import Image from 'next/image';
+import { HomeContainer, Product } from "@/styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat'
-})
+import camiseta1 from '@/assets/1.png'
+import camiseta2 from '@/assets/2.png'
+import camiseta3 from '@/assets/3.png'
 
 export default function Home() {
   return (
-    <Button>Botão</Button>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
