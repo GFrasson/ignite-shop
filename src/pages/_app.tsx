1f0361c7e46d6c105f2087860657c42c7e8f1412
Gabriel Frasson <bielfc27@gmail.com>
2023-04-13 12:02:08 -0300

    Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,21 @@
-import { globalStyles } from '@/styles/global'
+import Image from 'next/image';
 import type { AppProps } from 'next/app'
+
+import { globalStyles } from '@/styles/global'
+import { Container, Header } from '@/styles/pages/app';
+
+import logoImg from '@/assets/logo.svg';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <header>
-        
-      </header>
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
 
       <Component {...pageProps} />
-    </div>
+    </Container>
   )
 }
